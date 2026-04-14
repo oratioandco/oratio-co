b7081e443c424c0677f9161221ec634c9e87abfb
Tobias Treppmann <hello@treppmann.design>
2026-04-14 10:54:07 +0200

    Fix CSS loading, inline church SVG, fix base paths for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://oratio.co',
-  base: '/',
+  site: 'https://oratioandco.github.io',
+  base: '/oratio-co',
   output: 'static',
   integrations: []
 });
